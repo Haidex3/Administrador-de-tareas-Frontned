08c28794df044508cfa3d979091ded33e08bc43f
AndresSu2342 <cesar.borray-s@mail.escuelaing.edu.co>
2024-10-05 12:13:57 -0500

    Entradas de nuevos atributos

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -27,13 +27,19 @@ function openModal() {
 function saveTask() {
     const taskDescription = document.getElementById("taskDescription").value;
     if (taskDescription.trim() === '') return;
+    const taskPriority = document.getElementById("taskPriority").value;
+    if (taskPriority.trim() === '') return;
+    const taskLevelDifficult = document.getElementById("taskLevelDifficult").value;
+    if (taskLevelDifficult.trim() === '') return;
+    const taskAverageTime = document.getElementById("taskAverageTime").value;
+    if (taskAverageTime.trim() === '') return;
 
     fetch(apiUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ description: taskDescription, completed: false })
+        body: JSON.stringify({ description: taskDescription, completed: false, priority: taskPriority, difficultyLevel: taskLevelDifficult, averageDevelopmentTime: taskAverageTime })
     })
         .then(response => {
             if (!response.ok) {
