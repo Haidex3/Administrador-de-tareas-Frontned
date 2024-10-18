30f7262808db9ea9be68c626ce42dc76e997f5b5
AndresSu2342 <cesar.borray-s@mail.escuelaing.edu.co>
2024-10-18 01:58:50 -0500

    Reestructuracion Proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
+import TaskList from './components/TaskList';
+import Insights from './components/Insights';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={TaskList} />
+          <Route path="/insights" component={Insights} />
+        </Switch>
+      </Router>
   );
 }
 
